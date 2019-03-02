4899bc23fda1fa3fe21904e7616c3e230b942247
norawolf <noraevogt@gmail.com>
2019-03-02 16:50:43 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nora."
+  end
+
+  get '/hometown' do
+    "My hometown is Detroit!"
+  end
+
+  get('/favorite-song') {"My favorite song is a secret."}
+
 end
